fef591e1a6f97871cadfa0598bee5bb4f8e2d475
Hyoga Yoshida <hyoga0216@gmail.com>
2017-02-05 01:00:03 +0900

    ~2.65(2.64わからん)

--- src/exercise2_65.rkt
+++ src/exercise2_65.rkt
@@ -12,16 +12,29 @@
 				((> x (entry set)) (element-of-set? x (right-branch set)))))
 
 (define (adjoin-set x set)
-	(cond ((null? set) (make-tree x '() '()))
-				((= x (entry set)) set)
-				((< x (entry set)) 
-				 (make-tree (entry set)
-										(adjoin-set x (left-branch set))
-										(right-branch set)))
-				((> x (entry set))
-				 (make-tree (entry set)
-										(left-branch set)
-										(adjoin-set x (right-branch set))))))
+	(define branced-tree
+		(list->tree (tree->list set)))
+	(cond ((null? branced-tree) (make-tree x '() '()))
+				((= x (entry branced-tree)) branced-tree)
+				((< x (entry branced-tree)) 
+				 (make-tree (entry branced-tree)
+										(adjoin-set x (left-branch branced-tree))
+										(right-branch branced-tree)))
+				((> x (entry branced-tree))
+				 (make-tree (entry branced-tree)
+										(left-branch branced-tree)
+										(adjoin-set x (right-branch branced-tree))))))
+
+(define (tree->list tree)
+	(define (copy-to-list tree result-list)
+		(if (null? tree)
+			result-list
+			(copy-to-list (left-branch tree)
+										(cons (entry tree)
+													(copy-to-list
+														(right-branch tree)
+														result-list)))))
+	(copy-to-list tree '()))
 
 (define (list->tree elements)
 	(car (partical-tree elements (length elements))))
@@ -56,5 +69,6 @@
 																	 '()
 																	 '()))))
 
-;わからぬ
 (list->tree (list 1 3 5 7 9 11))
+
+(adjoin-set 12 tree1)
