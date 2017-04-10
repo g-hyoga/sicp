e4953ec6e951d4e15c90e8456675f17fa4ce94c3
g-hyoga <hyoga0216@gmail.com>
2017-04-10 10:37:53 +0900

    ....

--- src/exercise3_6.rkt
+++ src/exercise3_6.rkt
@@ -1,1 +1,20 @@
 #lang racket
+(require "package/random.rkt")
+
+(define (rand-generater)
+  (let ((x 1))
+    (define (generate)
+      (set! x (rand-update x))
+      x)
+    (define (reset v)
+      (set! x v))
+  
+    (define (dispatch p)
+      (cond ((eq? p 'generate) (generate))
+            ((eq? p 'reset) reset)))
+    dispatch))
+
+(define gene (rand-generater))
+(gene 'generate)
+((gene 'reset) 1)
+(gene 'generate)
