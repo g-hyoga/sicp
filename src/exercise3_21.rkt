67ddd3d4ff6f7c944a672e7d2b6fe13cb09c29f2
g-hyoga <hyoga0216@gmail.com>
2017-05-23 20:36:32 +0900

    ..

---
+++ src/exercise3_21.rkt
@@ -0,0 +1,25 @@
+#lang planet neil/sicp
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queur) (cdr queue))
+(define (set-front-ptr! queue item)
+	(set-car! queue item))
+(define (set-rear-ptr! queue item)
+	(set-cdr! queue item))
+(define (empty-queue? queue item)
+	(null? (front-ptr queue)))
+(define (front-queue queue)
+	(if (empty-queue? queue)
+		(error "FRONT called with an empty queue" queue)
+		(car (front-ptr queue))))
+(define (insert-queue! queue item)
+	(let ((new-pair (cons item '())))
+		(cond ((empty-queue? queue)
+					 (set-front-ptr! queue new-pair)
+					 (set-rear-ptr! queue new-pair)
+					 queue)
+					(else 
+						(set-cdr! (rear-ptr queue) new-pair)
+						(set-rear-ptr! queue new-pair)
+						queue))))
+
