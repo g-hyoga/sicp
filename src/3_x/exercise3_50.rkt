cf735821eb026dc7daf9791d915caa4a7570e706
g-hyoga <hyoga0216@gmail.com>
2017-10-07 20:00:26 +0900

    ...

--- src/3_x/exercise3_50.rkt
+++ src/3_x/exercise3_50.rkt
@@ -1,6 +1,23 @@
 #lang planet neil/sicp
 
+(define (memo-proc proc)
+	(let ((already-run? #f) (result #f))
+		(lambda ()
+			(if (not already-run?)
+				(begin (set! result (proc))
+							 (set! already-run? true)
+							 result)
+				result))))
 
+(define (delay delayed-object)
+	(memo-proc delayed-object))
 
+(define (stream-map proc . argstreams)
+	(if (strem-null? (car argstreams))
+		the-empty-stream
+		(cons-stream
+			(apply proc (map car argstreams))
+			(apply stream-map
+						 (cons proc (map cdr argstreams))))))
 
 
---
+++ src/3_x/exercise3_51.rkt
@@ -0,0 +1,62 @@
+#lang planet neil/sicp
+
+(define (strem-null? streams)
+	(= streams '()))
+
+(define (cons-stream a b)
+	(cons a (delay b)))
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-enumerate-interval low high)
+	(if (> low high)
+		the-empty-stream
+		(cons-stream
+			low
+			(stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+	(cond ((stream-null? stream) the-empty-stream)
+				((pred (stream-car stream))
+				 (cons-stream (stream-car stream)
+											(stream-filter
+												pred
+												(stream-cdr stream))))
+				(else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-for-each proc s)
+	(if (stream-null? s)
+		'done
+		(begin (proc (stream-car s))
+					 (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-ref s n)
+	(if (= n 0)
+		(stream-car s)
+		(stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+	(if (strem-null? (car argstreams))
+		the-empty-stream
+		(cons-stream
+			(apply proc (map car argstreams))
+			(apply stream-map
+						 (cons proc (map cdr argstreams))))))
+
+(define (display-line x)
+	(display x)
+	(display "\n"))
+
+(define (show x)
+	(display-line x)
+	x)
+
+
+(define x
+	(stream-map show
+							(stream-enumerate-interval 0 10)))
+
+(stream-ref x 5)
+(stream-ref x 7)
+
