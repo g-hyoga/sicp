6007ca6777a0e40bfb51965a3a5a45a7a3d0195a
g-hyoga <hyoga0216@gmail.com>
2017-10-19 13:40:31 +0900

    3_50

--- src/3_x/exercise3_50.rkt
+++ src/3_x/exercise3_50.rkt
@@ -6,40 +6,40 @@
 
 (define strem-null? stream-empty?)
 
-(define the-empty-stream (stream '()))
+(define the-empty-stream empty-stream)
 
 (define (cons-stream a b)
-  (stream-cons a b))
+	(stream-cons a b))
 
 (define stream-car stream-first)
 (define stream-cdr stream-rest)
 
 (define (memo-proc proc)
-  (let ((already-run? #f) (result #f))
-    (lambda ()
-      (if (not already-run?)
-          (begin (set! result (proc))
-                 (set! already-run? true)
-                 result)
-          result))))
+	(let ((already-run? #f) (result #f))
+		(lambda ()
+			(if (not already-run?)
+				(begin (set! result (proc))
+							 (set! already-run? true)
+							 result)
+				result))))
 
 (define (delay delayed-object)
-  (memo-proc delayed-object))
+	(memo-proc delayed-object))
 
 (define (stream-map proc . argstreams)
-  (if (strem-null? (car argstreams))
-      the-empty-stream
-      (cons-stream
-       (apply proc (map stream-car argstreams))
-       (apply stream-map
-              (cons proc (map stream-cdr argstreams))))))
+	(if (strem-null? (car argstreams))
+		the-empty-stream
+		(cons-stream
+			(apply proc (map stream-car argstreams))
+			(apply stream-map
+						 (cons proc (map stream-cdr argstreams))))))
 
 (define (stream-enumerate-interval low high)
-  (if (> low high)
-      the-empty-stream
-      (cons-stream
-       low
-       (stream-enumerate-interval (+ low 1) high))))
+	(if (> low high)
+		the-empty-stream
+		(cons-stream
+			low
+			(stream-enumerate-interval (+ low 1) high))))
 
 ;-----------------------------------------------------------------
 
@@ -47,10 +47,10 @@
 (define s (stream-enumerate-interval 1 2))
 
 (stream-for-each (lambda (x) 
-                   (display x)
-                   (display "\n"))
-                 (stream-map (lambda (x)
-                                (+ x x))
-                             s))
+									 (display x)
+									 (display "\n"))
+								 (stream-map (lambda (x)
+															 (+ x x))
+														 s))
 
 
