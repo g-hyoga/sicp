b0f2c0a64c840a39c8ca3db2f7683b3c46e11767
g-hyoga <hyoga0216@gmail.com>
2017-12-12 22:11:13 +0900

    勉強会後

--- src/3_x/exercise3_65.rkt
+++ src/3_x/exercise3_65.rkt
@@ -2,7 +2,19 @@
 
 (require racket/stream)
 
-(define (ln2 n))
+(define (ln2-summands n)
+  (stream-cons (/ 1.0 n)
+               (stream-map - (ln2 (+ n 1)))))
 
-(define (sqrt x tolerance)
-  (stream-limit (sqrt-stream x) tolerance))
+(define ln2-stream
+  (partical-sums (ln2-summands 1)))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1))
+        (s2 (stream-ref s 2)))
+    (stream-cons (- s2 (/ (square (- s2 s1)))
+                    (+ s0 (* -2 s1) s2)))
+    (euler-transform (stream-rest s))))
+
+(euler-transform ln2)
