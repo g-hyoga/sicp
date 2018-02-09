9977f26c4371167f7380b5fccc1b2d9cfb6d9df7
g-hyoga <hyoga0216@gmail.com>
2018-02-10 01:40:50 +0900

    4.1.1

--- src/4_x/4.1.4.rkt
+++ src/4_x/4.1.4.rkt
@@ -6,6 +6,49 @@
     (eq? (car exp) tag)
     false))
 
+;;;;; 4.1.1 ;;;;;
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp) (make-procedure (lambda-parameters exp)
+                                       (lambda-body exp)
+                                       env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+          (error "Unknown expression tyle: EVAL" exp))))
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-environment
+             arguments
+             (procedure-environment procedure))))
+        (else
+          (error "Unknown procedure type: APPLY" procedure))))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+    '()
+    (cons (eval (first-operand exps) env)
+          (list-of-values (rest-operands exps) env))))
+
+
+
+;;;;; 4.1.2 ;;;;;
+
 ;;;;; 4.1.3 ;;;;;
 
 (define (true? x) (not (eq? x false)))
